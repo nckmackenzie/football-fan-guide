2666d3f0170d77d5f0fb6d86f217e9f1d4e5e126
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-04-23 10:33:11 +0300

    Added selected prop to highlight active nav item

--- components/layout/BasicListItem.js
+++ components/layout/BasicListItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { ListItem, ListItemIcon, ListItemText } from '@mui/material';
 
 export default function BasicListItem({ icon, text, href }) {
+  const { pathname } = useRouter();
   return (
     <Link href={href}>
       <a className="no-underline text-inherit">
-        <ListItem button>
+        <ListItem button selected={pathname === href}>
           <ListItemIcon>{icon}</ListItemIcon>
           <ListItemText primary={text} />
         </ListItem>
