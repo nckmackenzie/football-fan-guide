3c8e8dd4d105740f752882d8183ec96f82cb3139
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-04-25 11:54:02 +0300

    Added the children prop

--- components/ui/BasicAlert.js
+++ components/ui/BasicAlert.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 
-export default function BasicAlert({ type, message, sx }) {
+export default function BasicAlert({ type, message, sx, children, title }) {
   return (
     <Alert severity={type} sx={{ ...sx }}>
-      {message}
+      <AlertTitle>{title || ''}</AlertTitle>
+      {message || children}
     </Alert>
   );
 }
