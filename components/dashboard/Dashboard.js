5c8447d19f384b47e45cfd2f5025a1ef9ac7438f
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-04-25 14:16:21 +0300

    Loading team info

--- components/dashboard/Dashboard.js
+++ components/dashboard/Dashboard.js
@@ -9,32 +9,50 @@ import BasicAlert from '../ui/BasicAlert';
 import Typography from '@mui/material/Typography';
 import BasicButton from '../ui/BasicButton';
 import { Grid } from '@mui/material';
+import { options } from '../../utils/options';
+import SmallCard from '../ui/cards/SmallCard';
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
   const [userClub, setUserClub] = useState(39);
-  //   const router = useRouter();
+  const [clubData, setClubData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    let q;
     const fetchData = async () => {
       const userRef = collection(db, 'users');
-      const q = query(userRef, where('user', '==', user?.uid));
-      const querySnapshot = await getDocs(q);
-      if (querySnapshot.empty) {
-        setUserClub();
-      } else {
-        querySnapshot.forEach(doc => {
-          if (doc.data()) {
-            const { club } = doc.data();
-            setUserClub(club);
-          }
-        });
+      if (user) {
+        q = query(userRef, where('user', '==', user?.uid));
+        const querySnapshot = await getDocs(q);
+        if (querySnapshot.empty) {
+          setUserClub();
+        } else {
+          querySnapshot.forEach(doc => {
+            if (doc.data()) {
+              const { club } = doc.data();
+              setUserClub(club);
+            }
+          });
+        }
       }
     };
     fetchData();
     setIsLoading(false);
   }, [user]);
 
+  useEffect(() => {
+    if (userClub) {
+      fetch(
+        `https://api-football-v1.p.rapidapi.com/v3/teams?id=${userClub}&league=39&season=2021`,
+        options
+      )
+        .then(response => response.json())
+        .then(response => setClubData(response?.response))
+        .catch(err => console.error(err));
+    }
+  }, [userClub]);
+
+  console.log(clubData);
   return (
     <>
       {!isLoading && !userClub && (
@@ -53,7 +71,32 @@ export default function Dashboard() {
       )}
       <Stack spacing={4}>
         {isLoading && <BasicCircularProgress />}
-        <Grid container spacing={2}></Grid>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6} lg={4}>
+            <SmallCard
+              src={
+                clubData[0]?.team?.logo ||
+                'https://media.api-sports.io/football/leagues/39.png'
+              }
+              width={64}
+              height={64}
+              name={clubData[0]?.team?.name || 'Not defined'}
+              title="My Club"
+            />
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            <SmallCard
+              src={
+                clubData[0]?.venue?.image ||
+                'https://media.api-sports.io/football/leagues/39.png'
+              }
+              width={64}
+              height={64}
+              name={clubData[0]?.venue?.name || 'Not defined'}
+              title="Venue"
+            />
+          </Grid>
+        </Grid>
       </Stack>
     </>
   );
