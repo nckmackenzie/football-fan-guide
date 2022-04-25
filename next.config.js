d89b7fee84c06e8fb8c2f605b468ab2f300a7dda
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-04-25 14:09:15 +0300

    Addition of domain and env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['firebasestorage.googleapis.com', 'media.api-sports.io'],
+  },
+  env: {
+    X_RapidAPI_Host: process.env.X_RapidAPI_Host,
+    X_RapidAPI_Key: process.env.X_RapidAPI_Key,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
